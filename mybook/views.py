24d33fe0775fbf34bb667c330a8910975a769ce5
ea-Mitsuoka <yukihide.mitsuoka@dragon.jp>
2021-08-04 16:35:01 +0900

    TXT認証のためTOPページを変更5

--- mybook/views.py
+++ mybook/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponseRedirect
 
 def index(request):
     # return HttpResponseRedirect("https://django-cloudrun-i7gftfbecq-uc.a.run.app/cms/")
-    return HttpResponseRedirect("https://django-cloudrun-i7gftfbecq-uc.a.run.app/")
+    return HttpResponseRedirect("https://django-cloudrun-i7gftfbecq-uc.a.run.app/index.html")
+
+def google1b6070a40dfc5ddd(request):
+    # return HttpResponseRedirect("https://django-cloudrun-i7gftfbecq-uc.a.run.app/cms/")
+    return HttpResponseRedirect("https://django-cloudrun-i7gftfbecq-uc.a.run.app/index.html")
